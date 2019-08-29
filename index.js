5d6a6505e161dd977166c1541019e82294419807
avpadill <avpadill@ucalgary.ca>
2019-08-29 02:08:27 -0600

    Separating functions, geocoding text addresses and reformatting data for response

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 const express = require('express');
-const app = express()
+const app = express();
 const Papa = require('papaparse');
 const _ = require('lodash');
 const fs = require('fs');
+const rp = require('request-promise');
 
-const PORT = 3000;
+const PORT = 3001;
 const DATASET_LOCATION = './datasets/Film_Locations_in_San_Francisco.csv';
+
+let formattedData;
+
+main();
 
 app.get('/status', (req, res) => res.send('OK'));
 
-app.get('/locations', function(req, res) {
+app.get('/locations', async function(req, res) {
+    console.log('\n\n == Request query: ', req.query);
+
+    let foundEntry = _.find(formattedData, { 'Title': _.lowerCase(decodeURIComponent(req.query.title)) });
+    let locationData = await Promise.all(_.map(foundEntry.Locations, async (location) => {
+        return {
+            name: location,
+            location: await geocodeAddress(location)
+        };
+    }));
+
+    console.log('\n\n === locationData: ', locationData);
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        Title: foundEntry.Title,
+        Locations: locationData
+    }));
+});
+
+async function geocodeAddress(location){
+    //TODO: Fix query string syntax for this.
+    let options = {
+        method: 'GET',
+        uri: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&region=ca&key=AIzaSyDNk7in3pRazdod4nels3lv_7ElxZTtzvo`,
+        json: true
+    };
+    
+    return rp(options).then((response) => {
+        console.log('\n\n ==== Response: ', response);
+        if (_.isEmpty(response.results)) {
+            return;
+        }
+
+        return response.results[0].geometry.location;
+    }).catch((err) => {
+        console.log('\n\n === Error inside Geocoding: ', err);
+    });
+}
+
+app.listen(PORT, () => console.log(`Application is listening on port ${PORT}!`)); 
+
+function main() {
     fs.readFile(DATASET_LOCATION, 'utf8', function(err, contents) {
         
         // Parsing the CSV.
@@ -18,19 +66,12 @@ app.get('/locations', function(req, res) {
         });
 
         // Formatting the data in CSV into an array of objects.
-        let formattedData = _.chain(data)
+        formattedData = _.chain(data)
             .groupBy('Title')
             .map((value, key) => ({
-                 Title: key, 
-                 Locations: _.map(value, 'Locations')
+                 Title: _.lowerCase(key), 
+                 Locations: _.uniq(_.map(value, 'Locations'))
             }))
             .value();
-
-        console.log('\n\n formattedData: ',  formattedData[0]);
-        
-        res.setHeader('Content-Type', 'application/json');
-        res.send(formattedData);
     });
-});
-
-app.listen(PORT, () => console.log(`Application is listening on port ${PORT}!`)); 
+}
